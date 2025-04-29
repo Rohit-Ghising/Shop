964e1143f7609fa663a0de0e2a7d5547e744e11c
Rohit-Ghising <ghisingrohit48@gmail.com>
2025-04-29 17:56:46 +0545

    Edit done for Product

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,6 +15,8 @@ import MyOrdersPage from "./pages/MyOrdersPage";
 import AdminLayout from "./components/Admin/AdminLayout";
 import AdminHomePage from "./pages/AdminHomePage";
 import UserManagement from "./components/Admin/UserManagement";
+import ProductManagement from "./components/Admin/ProductManagement";
+import EditProductPage from "./components/Admin/EditProductPage";
 
 const App = () => {
   return (
@@ -25,27 +27,29 @@ const App = () => {
           {/* for user */}
           <Route path="/" element={<UserLayout />}>
             <Route index element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/profile" element={<Profile />} />
+            <Route path="login" element={<Login />} />
+            <Route path="register" element={<Register />} />
+            <Route path="profile" element={<Profile />} />
             <Route
-              path="/collections/:collection"
+              path="collections/:collection"
               element={<CollectionPage />}
             />
-            <Route path="/product/:id" element={<ProductDetails />} />
-            <Route path="/checkout" element={<Checkout />} />
+            <Route path="product/:id" element={<ProductDetails />} />
+            <Route path="checkout" element={<Checkout />} />
             <Route
-              path="/order-confirmation"
+              path="order-confirmation"
               element={<OrderConfirmationPage />}
             />
-            <Route path="/order:id" element={<OrderDetailsPage />} />
-            <Route path="/my-orders" element={<MyOrdersPage />} />
+            <Route path="order:id" element={<OrderDetailsPage />} />
+            <Route path="my-orders" element={<MyOrdersPage />} />
           </Route>
           {/* for admon */}
 
           <Route path="/admin" element={<AdminLayout />}>
             <Route index element={<AdminHomePage />} />
             <Route path="users" element={<UserManagement />} />
+            <Route path="products" element={<ProductManagement />} />
+            <Route path="products/:id/edit" element={<EditProductPage />} />
           </Route>
         </Routes>
       </BrowserRouter>
