164888706748d56008fa1d068240eef1d74f10f6
Rohit-Ghising <ghisingrohit48@gmail.com>
2025-05-02 20:46:22 +0545

    home route done

--- backend/Routes/productRoutes.js
+++ backend/Routes/productRoutes.js
@@ -105,6 +105,80 @@ router.delete("/:id",protect,admin, async(req,res)=>{
   }
 })
 
+//get route
+//get all products with optional query
+
+router.get('/',async(req,res)=>{
+  try {
+    const {collection, size,color,gender,minPrice,maxPrice,sortBy,search,category,material,brand,limit} = req.query
+    let query = {}
+
+    //Filter  if ()
+    if(collection && collection.toLocaleLowerCase()!=="all"){
+      query.collections = collection
+    }
+    if(category && category.toLocaleLowerCase()!=="all"){
+      query.category = category
+    }
+    if(material){
+      query.material ={$in:material.split(",")}
+    }
+    if(brand){
+      query.brand ={$in:brand.split(",")}
+    }
+    if(size){
+      query.sizes ={$in:size.split(",")}
+    }
+    if(color){
+      query.colors ={$in:[color]}
+    }
+    if(gender){
+      query.gender =gender
+    }
+    if(minPrice || maxPrice){
+      query.price ={}
+      if(minPrice) query.price.gte = Number(minPrice)
+        if(maxPrice) query.price.lte = Number(maxPrice)
+    }
+  if(search) {
+    query.$or = [{description:{$regx:search,$options:"i"}},
+      
+
+    ]
+  }
+  //SORTLogic
+  let sort ={}
+  if(sortBy){
+    switch(sortBy){
+      case"priceAsc":
+      sort ={price: 1};
+      break;
+      case"priceDesc":
+      sort={price:-1}
+      break;
+      case"popularity":
+      sort ={rating:-1}
+      break 
+      default:
+        break
+
+    }
+  }
+  //Fetch the products from database
+  let products = await Product.find(query).sort(sort).limit(Number(limit)|| 0);
+  res.json(products)
+
+
+
+    
+  } catch (error) {
+    console.error(error)
+    res.status(500).send("Server Error");
+    
+    
+  }
+})
+
 
 module.exports = router
 
@@ -113,6 +187,5 @@ module.exports = router
 
 
 
-module.exports = router
 
 
--- backend/seeder.js
+++ backend/seeder.js
@@ -1,6 +1,45 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
 const Product = require("./models/Product")
-
 const User = require("./models/user")
+const products = require('./data/products')
 
+
+dotenv.config()
+
+mongoose.connect(process.env.MONGO_URL)
+
+//Function to seed data
+const seedData = async ()=>{
+  try {
+    //clerar existing data
+    await Product.deleteMany()
+    await User.deleteMany()
+    //Create a default admin
+    const createdUser = await User.create(
+      {
+        name:"Admin",
+        email:"admin@gmail.com",
+        password:"123456",
+        role:"admin"
+ 
+     }
+    )
+   //Assign degault userid to each product
+   const userID = createdUser._id
+   const sampleProducts =products.map((product)=>{
+    return {...product, user:userID}
+   })
+   //Insert data in DB
+  await Product.insertMany(sampleProducts)
+  console.log("Product data seeded sucessfully")
+  process.exit()
+
+  } catch (error) {
+    console.error("Error seeding the data",error);
+    process.exit(1)
+    
+    
+  }
+}
+seedData()
